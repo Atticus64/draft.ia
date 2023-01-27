6c62de26d2efb86c811d608dc0224c9b4199aa80
Jonathan <jonathanelian64@gmail.com>
2023-01-27 16:25:38 -0600

    Add config for deno deploy astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: deno()
 });
