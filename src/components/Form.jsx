639270dc73833eb3ca972381e2a4c59654191e84
Jonathan <jonathanelian64@gmail.com>
2023-02-03 11:59:43 -0700

    Fix bug mobile space width

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,6 @@ import { Button } from "./Button"
 import React from "react"
 import { Draft } from "./Draft"
 import '../css/Form.css' 
-import { Modal } from "./Modal"
 import { TopicContext } from "../context/TopicContext"
 import { useContext } from "react"
 import { useForm } from "react-hook-form"
@@ -95,10 +94,10 @@ export default function Form(){
   
   return (
     <>
-      <section className="grid max-w-sm">
+      <section className="grid">
           <wc-toast></wc-toast>
           <h2 className="font-semibold text-3xl">Draft on the fly!!</h2>
-          <form className="grid place-content-center ">
+          <form className="grid place-content-center">
             <label htmlFor="topic" className="mt-2">Topic</label>
             <textarea id="topic" required {...register("name")} style={{"resize": "none"}} autoFocus placeholder="type your topic, like neovim editor, history of rustlang, alamo usa" className="w-80 h-24 p-4 input" type="text" /> 
             <label htmlFor="prompt" className="mt-2">Prompt</label>
@@ -107,12 +106,12 @@ export default function Form(){
             <blockquote className="p-2 justify-center align-middle prompt-preview border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800" id="prompt" >
               <p className="prompt-user bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500">{iaPrompt}{data}</p>
             </blockquote>
-            <button className="p-4 hint--right" onClick={handleEditPrompt} aria-label="Edit prompt">
+            <button className="p-4 hint--top" onClick={handleEditPrompt} aria-label="Edit prompt">
               <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 256 256"><path fill="currentColor" d="m229.7 58.3l-32-32a8.1 8.1 0 0 0-11.4 0l-96 96A8.1 8.1 0 0 0 88 128v32a8 8 0 0 0 8 8h32a8.1 8.1 0 0 0 5.7-2.3l96-96a8.1 8.1 0 0 0 0-11.4Zm-105 93.7H104v-20.7l64-64L188.7 88ZM200 76.7L179.3 56L192 43.3L212.7 64Zm24 43.3v88a16 16 0 0 1-16 16H48a16 16 0 0 1-16-16V48a16 16 0 0 1 16-16h88a8 8 0 0 1 0 16H48v160h160v-88a8 8 0 0 1 16 0Z"/></svg>
             </button>
           </div>
         </section>
-        <div className="flex align-bottom">
+        <div className="flex">
           <Button onClick={handleClick} className="w-80 send activated">
             Send
           </Button>
